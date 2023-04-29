41a57021a57b61a4a1f7119cc8044f821ca2ce61
sanderkob <35116372+sanderkob@users.noreply.github.com>
2023-04-29 10:21:35 +0200

    update JsonArray sizing

--- software/NRG_itho_wifi/main/tasks/task_web.cpp
+++ software/NRG_itho_wifi/main/tasks/task_web.cpp
@@ -36,10 +36,6 @@ String debugVal = "";
 bool onOTA = false;
 bool TaskWebStarted = false;
 
-// Define JSON array to accumulate all Itho settings (in function getIthoSettingsBackupJSONPlus)
-DynamicJsonDocument content(10000); // Is sufficient for 13 kB file
-JsonArray sumJson = content.to<JsonArray>(); // Create Json array
-
 static const struct packed_files_lst
 {
   const char *name;
@@ -875,13 +871,14 @@ void handlePrevLogDownload(AsyncWebServerRequest *request)
 /*
 handleIthosettingsDownload() is called from server.on("/getithosettings",..
 The string /getithosettings is generated in html_ithosettings.html when the button 'Download IthoSettings.json' is pressed
-The Json array sumJson (extern declared in globals.h) is generated in getIthoSettingsBackupJSONPlus.cpp (in generic_functions.cpp).
+The Json array sumJson is filled in getIthoSettingsBackupJSONPlus.cpp (in generic_functions.cpp).
 The array sumJson is serialized and sent to  file IthoSettings.json, that then is offered for download.
 */
 void handleIthosettingsDownload(AsyncWebServerRequest *request)
 {
-  sumJson.clear(); // Clear JsonArray, but this leaves memory occupied
-  content.garbageCollect(); // Therefor clear leftover Jsonarray memory
+  // Define JSON array to accumulate all Itho settings (in function getIthoSettingsBackupJSONPlus)
+  DynamicJsonDocument content(110*currentIthoSettingsLength()); // e.g. 10000 is sufficient for 100 indexes, 13 kB file
+  JsonArray sumJson = content.to<JsonArray>(); // Create Json array
   File file = ACTIVE_FS.open("/IthoSettings.json", "w+"); // Create empty file
   if (!file)
   {
